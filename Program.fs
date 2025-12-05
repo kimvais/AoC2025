f4fce02ebbe8090e14b3111c0b8c36a82791f99a
Kimmo Parviainen-Jalanko <kimmo@vainu.io>
2025-12-05 19:41:54 +0200

    Make the output easier to copy

--- Program.fs
+++ Program.fs
@@ -14,22 +14,24 @@ let main argv =
     let day = argv |> getProblem
     let stopWatch = System.Diagnostics.Stopwatch.StartNew()
 
-    match day with
-    | "1" -> Day1.part1 "1" ()
-    | "1b" -> Day1.part2 "1" ()
-    | "2" -> Day2.part1 "2" ()
-    | "2b" -> Day2.part2 "2" ()
-    | "3" -> Day3.part1 "3" ()
-    | "3b" -> Day3.part2 "3" ()
-    | "4" -> Day4.part1 "4" ()
-    | "4b" -> Day4.part2 "4" ()
-    | "5" -> Day5.part1 "5" ()
-    | "5b" -> Day5.part2 "5" ()
-    | "6" -> Day6.part1 "6" ()
-    | "6b" -> Day6.part2 "6" ()
-    | "test" -> Day6.part1 "test6" ()
-    |> printfn "%d"
+    let result = 
+        match day with
+        | "1" -> Day1.part1 "1" ()
+        | "1b" -> Day1.part2 "1" ()
+        | "2" -> Day2.part1 "2" ()
+        | "2b" -> Day2.part2 "2" ()
+        | "3" -> Day3.part1 "3" ()
+        | "3b" -> Day3.part2 "3" ()
+        | "4" -> Day4.part1 "4" ()
+        | "4b" -> Day4.part2 "4" ()
+        | "5" -> Day5.part1 "5" ()
+        | "5b" -> Day5.part2 "5" ()
+        | "6" -> Day6.part1 "6" ()
+        | "6b" -> Day6.part2 "6" ()
+        | "test" -> Day6.part1 "test6" ()
+        | s -> failwith $"Invalid choice {s}"
 
     stopWatch.Stop()
-    printfn "Ran for %0.3f seconds" stopWatch.Elapsed.TotalSeconds
+    printfn "Ran for %0.3f seconds\n" stopWatch.Elapsed.TotalSeconds
+    printfn "%d\n" result
     0
