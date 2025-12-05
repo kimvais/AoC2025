88a599d16176a2efade1bde628d95cbe3cb71d94
Kimmo Parviainen-Jalanko <kimmo@vainu.io>
2025-12-05 17:25:55 +0200

    Refactor day 4 solution for part 2

--- Day4.fs
+++ Day4.fs
@@ -7,47 +7,37 @@ let hasPaper =
     | '@' -> true
     | _ -> false
 
-let findNeighbours (arr: 'a array array) x y =
-    seq {
-        if y > 0 then
-            if x > 0 then
-                yield arr.[y - 1][x - 1]
-
-            yield arr.[y - 1][x]
-
-            if x < Array.length arr - 1 then
-                yield arr.[y - 1][x + 1]
-
-        if x > 0 then
-            yield arr.[y][x - 1]
-
-        if x < Array.length arr - 1 then
-            yield arr.[y][x + 1]
-
-        if y < Array.length arr - 1 then
-            if x > 0 then
-                yield arr.[y + 1][x - 1]
-
-            yield arr.[y + 1][x]
-
-            if x < Array.length arr - 1 then
-                yield arr.[y + 1][x + 1]
-    }
+let getNeighbourCoords maxX maxY x y =
+    let inBounds x'' y'' =
+        0 <= x'' && x'' <= maxX && 0 <= y'' && y'' <= maxY
+    let deltas = [
+          (-1, -1); (-1, 0); (-1, 1);
+          ( 0, -1);          ( 0, 1);
+          ( 1, -1); ( 1, 0); ( 1, 1);
+    ]
+    deltas |> Seq.choose (fun (dy, dx) ->
+        let y' = y + dy
+        let x' = x + dx
+        if inBounds x' y' then Some (y', x') else None)
 
 let part1 fn () =
     let grid =
         readInput fn |> Array.ofSeq |> Array.map (Array.ofSeq >> Array.map hasPaper)
+
+    let getRoll (y, x) = grid.[y].[x]
+    let maxY = Array.length grid - 1
+    let maxX = Array.length grid.[0] - 1
 
     let rollCounts =
         seq {
             for y in [ 0 .. (Array.length grid) - 1 ] do
                 for x in [ 0 .. (Array.length grid.[0]) - 1 ] do
                     if grid.[y].[x] then
-                        yield findNeighbours grid x y
+                        yield getNeighbourCoords maxX maxY x y
         }
 
     rollCounts
-    |> Seq.map (Seq.filter id >> Seq.length)
+    |> Seq.map (Seq.filter getRoll >> Seq.length)
     |> Seq.filter (fun n -> n < 4)
     |> Seq.length
     |> int64
